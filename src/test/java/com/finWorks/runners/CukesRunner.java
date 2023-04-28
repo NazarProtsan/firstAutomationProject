6703e4f9ee4c0dfff23cafec25d0744737288f0e
NazarProtsan <114443840+NazarProtsan@users.noreply.github.com>
2023-04-28 14:16:45 -0400

    US92 Completed

--- src/test/java/com/finWorks/runners/CukesRunner.java
+++ src/test/java/com/finWorks/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/finWorks/step_definitions",
         dryRun = false,
-        tags = "@regression",
+        tags = "@Evgenia",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
